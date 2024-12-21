4a73cc2fc915618b8ecb9f9f349a2bd0692cb3b4
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-12-22 01:21:44 +0530

    Create Calorie Intake.cpp

---
+++ Calorie Intake.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+      int x,y,z;
+      cin>>x>>y>>z;
+      int p =x-(y*z);
+      if (p>0) cout<<p<<endl;
+      else cout<<-1<<endl;
+
+}
