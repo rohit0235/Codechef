6620458d82cace1622d84a1718fc1ac1d84ed637
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-12-27 11:04:04 +0530

    Create Rectangle.cpp

---
+++ Rectangle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 
+	 int t;
+	 cin>>t;
+	 while (t--){
+	     int a,b,c,d;
+	     cin>>a>>b>>c>>d;
+	     
+	     if (a==b && c==d){
+	          cout<<"YES"<<endl;      
+	     }
+	    else if (a==c && b==d){
+	          cout<<"YES"<<endl;      
+	     }
+	    else if (a==d && c==b){
+	          cout<<"YES"<<endl;      
+	     }
+	      else{
+	          cout<<"NO"<<endl;
+	      }
+	     
+	 }
+	 
+	 
+	 
+
+}
