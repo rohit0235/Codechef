23d67a1a39576f36ad38bbbd7d19bafb67f669d7
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-12-30 01:37:08 +0530

    Create Magician versus Chef.cpp

---
+++ Magician versus Chef.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        
+        int t;
+        cin>>t;
+        while(t--){
+            int n,x,s;
+            cin>>n>>x>>s;
+        //   set<int>swaps;
+        
+             while (s--){
+                 int a,b;
+                 cin>>a>>b;
+                 if (x==a){
+                x=b;
+                  
+                 }
+                 else if (x==b){
+                   x=a;
+                  
+                 }
+                 
+                 
+             }
+             cout<<x<<endl;
+        }
+
+}
