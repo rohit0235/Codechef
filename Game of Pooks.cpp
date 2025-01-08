d4d392e9184e09f444929d1f8a6c340902e5060a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-08 18:36:20 +0530

    Create Game of Pooks.cpp

---
+++ Game of Pooks.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+          int t;
+          cin>>t;
+          while (t--){
+              int n;
+              cin>>n;
+              if (n==1)  cout<<1<<endl;
+              else if (n<=3) cout<<n-1<<endl;
+              else cout<<n<<endl;
+         
+          }
+}
