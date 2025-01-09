34e4ecb99d364c7621c83d7b984534a11e0f3094
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-09 10:50:10 +0530

    Create Fit in Data Type.cpp

---
+++ Fit in Data Type.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+      int t;
+      cin>>t;
+      while (t--){
+          int n,x;
+          cin>>n>>x;
+        //   if (n>x) cout<<x<<endl;
+        //   else {
+              cout<<x%(n+1)<<endl;
+        //   }
+      }
+}
